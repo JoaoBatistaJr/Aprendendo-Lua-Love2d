7758f33f5585568cc816b18e5cfe851fb47251fb
joaobatistajr <joaob.dev@gmail.com>
2025-01-01 23:11:22 -0300

    aprendendo a depurar

--- main.lua
+++ main.lua
@@ -1,15 +1,38 @@
-function  love.load()
-    song = love.audio.newSource("song.ogg", "stream")
+io.stdout:setvbuf("no")
 
-    love.audio.play(song)
-    song:setLooping(true)
-    song:play()
 
-    sfx = love.audio.newSource("sfx.ogg", "static")
+function love.load()
+    circle = {
+        x = 100,
+        y = 100
+    }
+    bullets = {}
+end
+
+function love.update(dt)
+    for i, v in ipairs(bullets) do
+        v.x = v.x + 400 * dt
+        print(x,y)
+    end
+end
+
+function love.draw()
+    love.graphics.circle("fill", circle.x, circle.y, 50)
+
+    for i, v in ipairs(bullets) do
+        love.graphics.circle("fill", v.x, v.y, 10)
+    end
 end
 
 function love.keypressed(key)
+  print("whats is the key?", key)
     if key == "space" then
-        sfx:play()
+        shoot()
     end
+end
+
+function shoot()
+    table.insert(bullets, {x = circle.x, y = circle.y})
+
+    print("How many bullets", #bullets)
 end
---
+++ main_audio.lua
@@ -0,0 +1,15 @@
+function  love.load()
+    song = love.audio.newSource("song.ogg", "stream")
+
+    love.audio.play(song)
+    song:setLooping(true)
+    song:play()
+
+    sfx = love.audio.newSource("sfx.ogg", "static")
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        sfx:play()
+    end
+end
